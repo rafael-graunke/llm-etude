7ebcb81ea09a57db4ea1d3c30a2fca0d83d3b146
Rafael Graunke <rafael.graunke@outlook.com>
2026-09-09 00:39:10 -0300

    adds tool use section

--- src/constants.py
+++ src/constants.py
@@ -1,5 +1,6 @@
 import os
 
 MODEL = os.getenv("MODEL", "ollama/qwen3:14b")
+CHAT_MODEL = os.getenv("MODEL", "ollama_chat/qwen3:14b")
 DATASET_MODEL = os.getenv("DATASET_MODEL", "ollama/llama3.2:3b")
 GRADING_MODEL = os.getenv("GRADING_MODEL", "ollama/phi4-reasoning:14b")
